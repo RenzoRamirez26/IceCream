31249f1dc6d82430f17765113df9305cff0274fd
Jesus Lorenzo Ramirez Perales <lorenzor@edul.edu>
2024-08-07 14:01:40 -0400

    created make nt

---
+++ docs/make.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.45
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 9428168e-2b82-42af-a1ed-b6903bd40fc8
+
+
+# ╔═╡ Cell order:
+# ╠═9428168e-2b82-42af-a1ed-b6903bd40fc8
