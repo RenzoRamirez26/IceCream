30ae79e36965f670c434d79444b65eecab0511ec
Jesus Lorenzo Ramirez Perales <lorenzor@edul.edu>
2024-08-07 14:37:17 -0400

    more idk chnages

---
+++ docs/build/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#CheeseCake.jl","page":"Home","title":"CheeseCake.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"The best summation package.","category":"page"},{"location":"#Package-Features","page":"Home","title":"Package Features","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Sum the squares of two numbers","category":"page"}]
+}
