d48b5f2598fb8536429c79a2a087957354ba36fa
Jesus Lorenzo Ramirez Perales <lorenzor@edul.edu>
2024-08-07 15:14:17 -0400

    changes

---
+++ ApplePie.jl
@@ -0,0 +1,14 @@
+### A Pluto.jl notebook ###
+# v0.19.45
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ f3ce3202-54f0-11ef-0ed5-1770c22b9f7c
+begin
+	R = rand(500,500)
+	M = R' * R
+end
+
+# ╔═╡ Cell order:
+# ╠═f3ce3202-54f0-11ef-0ed5-1770c22b9f7c
